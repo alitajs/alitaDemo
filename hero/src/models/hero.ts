8e28c494a752428259aba9b1556e9d3a0d867dba
chenshuhang <437132124@qq.com>
2020-05-24 16:54:01 +0800

    feat: alita demo listen router

--- hero/src/models/hero.ts
+++ hero/src/models/hero.ts
@@ -1,16 +1,15 @@
 import { Reducer } from 'redux';
-import { query } from '@/services/api';
-import { Effect } from '@/models/connect';
+import { Effect } from 'alita';
 
 export interface HeroModelState {
-  name: string;
+  heros: any;
 }
 
 export interface HeroModelType {
   namespace: 'hero';
   state: HeroModelState;
   effects: {
-    query: Effect;
+    fetch: Effect;
   };
   reducers: {
     save: Reducer<HeroModelState>;
@@ -21,16 +20,34 @@ const HeroModel: HeroModelType = {
   namespace: 'hero',
 
   state: {
-    name: '',
+    heros: [],
   },
 
   effects: {
-    *query({ payload }, { call, put }) {
-      const data = yield call(query, payload);
-      console.log(data)
+    *fetch(_, { put }) {
+      const data = [
+        {
+          ename: 105,
+          cname: '廉颇',
+          title: '正义爆轰',
+          new_type: 0,
+          hero_type: 3,
+          skin_name: '正义爆轰|地狱岩魂',
+        },
+        {
+          ename: 106,
+          cname: '小乔',
+          title: '恋之微风',
+          new_type: 0,
+          hero_type: 2,
+          skin_name: '恋之微风|万圣前夜|天鹅之梦|纯白花嫁|缤纷独角兽',
+        },
+      ];
       yield put({
         type: 'save',
-        payload: { name: data.text },
+        payload: {
+          heros: data,
+        },
       });
     },
   },
--- hero/src/pages/hero/index.tsx
+++ hero/src/pages/hero/index.tsx
@@ -10,7 +10,7 @@ const HeroPage: FC<PageProps> = ({ hero, dispatch }) => {
   // 这里发起了初始化请求
   useEffect(() => {
     dispatch!({
-      type: 'hero/query',
+      type: 'hero/fetch',
     });
     return () => {
       // 这里写一些需要消除副作用的代码
@@ -18,8 +18,12 @@ const HeroPage: FC<PageProps> = ({ hero, dispatch }) => {
     };
   }, []);
   // 注意，上面这里写空数组，表示初始化，如果需要监听某个字段变化再发起请求，可以在这里写明
-  const { name } = hero;
-  return <div className={styles.center}>Hello {name}</div>;
+  const { heros } = hero;
+  return (
+    <div className={styles.center}>
+      <h2>This is {JSON.stringify(heros)}</h2>
+    </div>
+  );
 };
 
-export default connect(({ hero }:{ hero: HeroModelState; }) => ({ hero }))(HeroPage);
+export default connect(({ hero }: { hero: HeroModelState }) => ({ hero }))(HeroPage);
