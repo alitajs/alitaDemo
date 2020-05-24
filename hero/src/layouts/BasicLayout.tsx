6d5dfff410c28fa828fd8e6c1793cc9fc3d317f3
chenshuhang <437132124@qq.com>
2020-05-24 16:38:42 +0800

    feat: alita demo navigation to page

--- hero/src/layouts/BasicLayout.tsx
+++ hero/src/layouts/BasicLayout.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { Link } from 'alita';
 import { Layout, Menu } from 'antd';
 import styles from './index.less';
 
 const { Header, Content, Footer } = Layout;
 
-const BasicLayout: React.FC = ({ children }) => (
+const menuData = [
+  { route: 'hero', name: '英雄' },
+  { route: 'item', name: '局内道具' },
+  { route: 'summoner', name: '召唤师技能' },
+];
+
+const BasicLayout: React.FC = ({ children, location: { pathname } }) => (
   <Layout>
     <Header>
       <div className={styles.logo}>王者荣耀资料库 </div>
       <Menu
         theme="dark"
         mode="horizontal"
-        defaultSelectedKeys={['1']}
+        defaultSelectedKeys={[pathname]}
         style={{ lineHeight: '64px' }}
       >
-        <Menu.Item key="1">英雄</Menu.Item>
-        <Menu.Item key="2">局内道具</Menu.Item>
-        <Menu.Item key="3">召唤师技能</Menu.Item>
+        {menuData.map((menu) => (
+          <Menu.Item key={`/${menu.route}`}>
+            <Link to={menu.route}>{menu.name}</Link>
+          </Menu.Item>
+        ))}
       </Menu>
     </Header>
     <Content style={{ padding: '0 50px' }}>
